5d49ed6cce5a23e00e9605ca5f12c7ea140d86d2
OukaMiyuki <asukaloid.blog@gmail.com>
2020-07-28 08:58:25 +0700

    Panambahan show data mahasiswa

--- Controller/mahasiswa.js
+++ Controller/mahasiswa.js
@@ -1,12 +1,33 @@
 'use strict';
 
 const response = require('../res');
-const connection = require('../Server/DB');
+const conn = require('../Server/DB');
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
     response.ok("Aplikasi Berjalan!", res)
+});
+
+router.get('/show_data', async (req, res) => {
+    await conn.query('SELECT * FROM mahasiswa', (err, rows, fields) =>{
+        if(err){
+            console.log(err);
+        } else{
+            response.ok(rows, res);
+        }
+    });
+});
+
+router.get('/show_data/profile/:id', async (req, res) => {
+    let idMahasiswa = req.params.id;
+    await conn.query('SELECT * FROM mahasiswa WHERE nim = ?', [idMahasiswa], (err, rows, fields) =>{
+        if(err){
+            console.log(err);
+        } else{
+            response.ok(rows, res);
+        }
+    });
 });
 
 module.exports = router;
